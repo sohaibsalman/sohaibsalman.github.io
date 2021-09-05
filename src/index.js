8a20fe7ea96037b7a1794f338aac6d0b22f39d8b
Sohaib Salman <sohaibsalman10@gmail.com>
2021-09-05 13:18:08 +0500

    Setup react app

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core";
 
-import "boxicons";
-
-import theme from "./themes/theme";
 import App from "./App";
 
 import "./index.css";
 
-import "devicon/devicon.min.css";
-
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <ThemeProvider theme={theme}>
+    <React.StrictMode>
         <App />
-      </ThemeProvider>
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    </React.StrictMode>,
+    document.getElementById("root")
 );
